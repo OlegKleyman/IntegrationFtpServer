b47ad37bf37f836855ff29a48121b9f0fb2d97b2
Oleg Kleyman <Omego2K@gmail.com>
2016-09-11 12:05:26 -0400

    add FtpConfiguration constructor tests

--- core/Omego.SimpleFtp/FtpConfiguration.cs
+++ core/Omego.SimpleFtp/FtpConfiguration.cs
@@ -1,10 +1,22 @@
 ﻿namespace Omego.SimpleFtp
 {
+    using System;
+
     public class FtpConfiguration
     {
+        public string HomeDirectory { get; private set; }
+
+        public int Port { get; private set; }
+
         public FtpConfiguration(string homeDirectory, int port)
         {
-            
+            if (homeDirectory == null) throw new ArgumentNullException(nameof(homeDirectory));
+            if (homeDirectory.Length == 0) throw new ArgumentException("Value cannot be empty.", nameof(homeDirectory));
+            if (homeDirectory.Trim(' ').Length == 0) throw new ArgumentException("Value cannot be only whitespace.", nameof(homeDirectory));
+            if(port == 0) throw new ArgumentException("Value cannot be 0.", nameof(port));
+
+            HomeDirectory = homeDirectory;
+            Port = port;
         }
     }
 }
--- tests/unit/Omego.SimpleFtp.Tests.Unit/FtpConfigurationTests.cs
+++ tests/unit/Omego.SimpleFtp.Tests.Unit/FtpConfigurationTests.cs
@@ -1,9 +1,72 @@
 ﻿namespace Omego.SimpleFtp.Tests.Unit
 {
+    using System;
+    using System.Collections;
+
+    using FluentAssertions;
+
     using NUnit.Framework;
 
     [TestFixture]
     public class FtpConfigurationTests
     {
+        [TestCase("Home", 21)]
+        public void ConstructorShouldSetProperties(string homeDirectory, int port)
+        {
+            var config = new FtpConfiguration(homeDirectory, port);
+
+            config.HomeDirectory.ShouldBeEquivalentTo(homeDirectory);
+            config.Port.ShouldBeEquivalentTo(port);
+        }
+
+        [TestCaseSource(typeof(FtpConfigurationTestsSource),
+             nameof(FtpConfigurationTestsSource.ConstructorShouldThrowArgumentExceptionWhenArgumentsAreInvalidCases))]
+        public void ConstructorShouldThrowArgumentExceptionWhenArgumentsAreInvalid(
+            string homeDirectory,
+            int port,
+            string expectedParameterName,
+            string expectedMessage,
+            Type expectedType)
+        {
+            Action constructor = () => new FtpConfiguration(homeDirectory, port);
+
+            constructor.ShouldThrow<ArgumentException>()
+                .WithMessage(expectedMessage)
+                .Where(
+                    exception => exception.ParamName == expectedParameterName,
+                    "the parameter name should be of the problematic parameter")
+                .And.Should()
+                .BeOfType(expectedType);
+        }
+
+        private class FtpConfigurationTestsSource
+        {
+            public static IEnumerable ConstructorShouldThrowArgumentExceptionWhenArgumentsAreInvalidCases
+                =>
+                new[]
+                    {
+                        new object[]
+                            {
+                                null, default(int), "homeDirectory",
+                                "Value cannot be null.\r\nParameter name: homeDirectory", typeof(ArgumentNullException)
+                            },
+                        new object[]
+                            {
+                                string.Empty, default(int), "homeDirectory",
+                                "Value cannot be empty.\r\nParameter name: homeDirectory", typeof(ArgumentException)
+                            },
+                        new object[]
+                            {
+                                "  ", default(int), "homeDirectory",
+                                "Value cannot be only whitespace.\r\nParameter name: homeDirectory",
+                                typeof(ArgumentException)
+                            },
+                        new object[]
+                            {
+                                "test", default(int), "port", "Value cannot be 0.\r\nParameter name: port",
+                                typeof(ArgumentException)
+                            }
+                    };
+        }
     }
 }
