10c9e9d657e6aee0f6febdd34e4e45fee4633a63
Oleg Kleyman <Omego2K@gmail.com>
2016-09-11 18:16:54 -0400

    add test for Kill method

---
+++ tests/integration/Omego.SimpleFtp.Tests.Integration/SystemProcessTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Omego.SimpleFtp.Tests.Integration
+{
+    using System.Diagnostics;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SystemProcessTests
+    {
+        [Test]
+        public void KillShouldKillTheProcess()
+        {
+            var process = Process.Start("cmd.exe");
+
+            GetSystemProcess(process).Kill();
+
+            process.HasExited.Should().BeTrue();
+        }
+
+        public static SystemProcess GetSystemProcess(Process process)
+        {
+            return new SystemProcess(process);
+        }
+    }
+}
