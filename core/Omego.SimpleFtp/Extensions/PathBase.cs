dcf70be99949de014ce3a1284883faaafdf4ee8c
Oleg Kleyman <Omego2K@gmail.com>
2016-09-11 13:44:55 -0400

    throw exception if path is null

--- core/Omego.SimpleFtp/Extensions/PathBase.cs
+++ core/Omego.SimpleFtp/Extensions/PathBase.cs
@@ -1,5 +1,6 @@
 ﻿namespace Omego.SimpleFtp.Extensions
 {
+    using System;
     using System.IO;
 
     using PathAbstractions = System.IO.Abstractions.PathBase;
@@ -8,6 +9,8 @@
     {
         public static string GetTempFilePath(this PathAbstractions path, string extension)
         {
+            if(path == null) throw new ArgumentNullException(nameof(path));
+
             var tempDirectory = path.GetTempPath();
             var tempName = Path.GetFileNameWithoutExtension(path.GetTempFileName());
             var tempFileName = Path.ChangeExtension(tempName, extension);
--- tests/unit/Omego.SimpleFtp.Tests.Unit/Extensions/PathBaseTests.cs
+++ tests/unit/Omego.SimpleFtp.Tests.Unit/Extensions/PathBaseTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace Omego.SimpleFtp.Tests.Unit.Extensions
 {
+    using System;
+    using System.Collections;
     using System.IO.Abstractions.TestingHelpers;
 
     using FluentAssertions;
@@ -9,6 +11,8 @@
     using NUnit.Framework;
 
     using Omego.SimpleFtp.Extensions;
+
+    using PathBase = System.IO.Abstractions.PathBase;
 
     [TestFixture]
     public class PathBaseTests
@@ -21,6 +25,40 @@
             path.GetTempFileName().Returns("something");
 
             path.GetTempFilePath("exe").ShouldBeEquivalentTo(@"C:\temp\something\something.exe");
+        }
+
+        [TestCaseSource(typeof(PathBaseTestsSource),
+             nameof(PathBaseTestsSource.GetTempFilePathShouldThrowArgumentExceptionWhenArgumentsAreInvalidCases))]
+        public void GetTempFilePathShouldThrowArgumentExceptionWhenArgumentsAreInvalid(
+            string expectedParameterName,
+            string expectedMessage,
+            Type expectedType,
+            PathBase path,
+            string extension)
+        {
+            Action getTempFilePath = () => path.GetTempFilePath(extension);
+
+            getTempFilePath.ShouldThrow<ArgumentException>()
+                .WithMessage(expectedMessage)
+                .Where(
+                    exception => exception.ParamName == expectedParameterName,
+                    "the parameter name should be of the problematic parameter")
+                .And.Should()
+                .BeOfType(expectedType);
+        }
+
+        private class PathBaseTestsSource
+        {
+            public static IEnumerable GetTempFilePathShouldThrowArgumentExceptionWhenArgumentsAreInvalidCases
+                =>
+                new[]
+                    {
+                        new object[]
+                            {
+                                "path", "Value cannot be null.\r\nParameter name: path", typeof(ArgumentNullException),
+                                null, null
+                            }
+                    };
         }
     }
 }
